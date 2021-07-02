67f8385e544bfe5ff05729b05c35140cf2893bcc
下山化缘的小和尚 <72691225+workcjl@users.noreply.github.com>
2021-07-02 16:35:01 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: ''
+    }
+  },
+  created() {
+    this.getHome()
+  },
+  methods: {
+    getHome() {
+      axios.get('http://127.0.0.1:3000/api/home').then(({ data: { name } }) => {
+        this.name = name
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
